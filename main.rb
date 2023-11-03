614ede01395b9e1e6fa6bb7db4fe518e4da89f0f
Adeshina Adenuga <Nuga0909@gmail.com>
2023-11-03 10:28:20 +0100

    update main.rb file

--- main.rb
+++ main.rb
@@ -10,6 +10,3 @@ puts(list.all? { |e| e > 5 }) # Output: false
 # Testing for any? method
 puts(list.any? { |e| e == 2 }) # Output: true
 puts(list.any? { |e| e == 5 }) # Output: false
-
-# Testing for filter? method
-puts(list.filter { |e| e.even? }) # Output: [2, 4]
